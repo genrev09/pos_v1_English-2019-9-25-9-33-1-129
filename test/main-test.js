e07c201143b24ebe56361b50a121eb751b8874c9
GENREV ARAMBULO (SAPE-MIS-ISD-OOCLL/MNL) <ARAMBGE@OOCL.COM>
2019-10-11 09:30:34 +0800

    Committed Partial test for POS

--- test/main-test.js
+++ test/main-test.js
@@ -30,4 +30,146 @@ Discounted prices：7.50(yuan)
 
     expect(console.log).toHaveBeenCalledWith(expectText);
   });
+
+  it('should load items using decoded barcodes', () => {
+
+    const decodedBarcode = [
+      { barcode:'ITEM000001', count:3},
+      { barcode:'ITEM000003', count:1},
+    ];
+
+    const expectedResult = [{barcode: 'ITEM000001', name: 'Sprite', price: 3.00, unit:'bottle'},
+                            {barcode: 'ITEM000003', name: 'Litchi', price: 15.00, unit:'pound'}];
+
+    const actualResult = loadItems(decodedBarcode);
+
+    expect(actualResult).toEqual(expectedResult);
+  });
+
+  it('should decode barcodes from tags', () => {
+    const tags = [
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000003'
+    ];
+    
+  const expectedResult = [
+    { barcode:'ITEM000001', count:3},
+    { barcode:'ITEM000003', count:1},
+  ];
+
+  const actualResult = decodeBarcodes(tags);
+  
+  expect(actualResult).toEqual(expectedResult);
+  });
+
+  it('should combine items using decoded barcodes', () => {
+
+    const decodedBarcode = [
+      { barcode:'ITEM000001', count:3},
+      { barcode:'ITEM000003', count:1},
+    ];
+
+    const expectedResult = [{barcode: 'ITEM000001', name: 'Sprite', price: 3.00, unit:'bottle' , count: 3},
+                            {barcode: 'ITEM000003', name: 'Litchi', price: 15.00, unit:'pound', count: 1}];
+
+    const actualResult = combineItems(decodedBarcode);
+    
+    expect(actualResult).toEqual(expectedResult);
+  });
+
+  it('should decode tags and return an item of object', () => {
+      const tags = [
+        'ITEM000001',
+        'ITEM000001',
+        'ITEM000001',
+        'ITEM000003'
+      ];
+      
+      const expectedResult = [
+        {barcode: 'ITEM000001', name: 'Sprite', price: 3.00, unit:'bottle' , count: 3},
+        {barcode: 'ITEM000003', name: 'Litchi', price: 15.00, unit:'pound', count: 1}];
+
+    const actualResult = decodeBarcodes(tags);
+    
+    expect(actualResult).toEqual(expectedResult);
+  });
+
+  it('should load all promotions from database', () => {
+
+    const expectedResult = [
+      {
+        type: 'BUY_TWO_GET_ONE_FREE',
+        barcodes: [
+          'ITEM000000',
+          'ITEM000001',
+          'ITEM000005'
+        ]
+      }
+    ];
+  const actualResult = loadPromotions();
+  
+  expect(actualResult).toEqual(expectedResult);
+  });
+
+  it('should get subtotal of item in accordance to promotion', () => {
+    const items = [
+      {barcode: 'ITEM000001', name: 'Sprite', price: 3.00, unit:'bottle' , count: 3},
+      {barcode: 'ITEM000003', name: 'Litchi', price: 15.00, unit:'pound', count: 1}
+    ];
+    
+    const promotions = [
+      {
+        type: 'BUY_TWO_GET_ONE_FREE',
+        barcodes: [
+          'ITEM000000',
+          'ITEM000001',
+          'ITEM000005'
+        ]
+      }
+    ];
+    
+    const expectedResult = [
+      {barcode: 'ITEM000001', name: 'Sprite', price: 3.00, unit:'bottle' , count: 3, subtotal: 6.00},
+      {barcode: 'ITEM000003', name: 'Litchi', price: 15.00, unit:'pound', count: 1, subtotal: 15.00}];
+
+  const actualResult = computeSubtotal(items,promotions);
+  
+  expect(actualResult).toEqual(expectedResult);
+  });
+
+  it('should calculate receipt items', () => {
+    const items = [
+      {barcode: 'ITEM000001', name: 'Sprite', price: 3.00, unit:'bottle' , count: 3},
+      {barcode: 'ITEM000003', name: 'Litchi', price: 15.00, unit:'pound', count: 1}
+    ];
+    
+    const expectedResult = [
+      {barcode: 'ITEM000001', name: 'Sprite', price: 3.00, unit:'bottle' , count: 3, subtotal: 6.00},
+      {barcode: 'ITEM000003', name: 'Litchi', price: 15.00, unit:'pound', count: 1, subtotal: 15.00}];
+
+  const actualResult = calculateReceiptItems(items);
+  
+  expect(actualResult).toEqual(expectedResult);
+  });
+
+  it('should calculate receipt total', () => {
+    const receiptItems = [
+      {barcode: 'ITEM000001', name: 'Sprite', price: 3.00, unit:'bottle' , count: 3, subtotal: 6.00},
+      {barcode: 'ITEM000003', name: 'Litchi', price: 15.00, unit:'pound', count: 1, subtotal: 15.00}];
+
+    
+    const expectedResult = [
+      [{barcode: 'ITEM000001', name: 'Sprite', price: 3.00, unit:'bottle' , count: 3, subtotal: 6.00},
+       {barcode: 'ITEM000003', name: 'Litchi', price: 15.00, unit:'pound', count: 1, subtotal: 15.00}],
+       {total : 21},
+       {subtotal: 0}
+    ];
+
+  const actualResult = calculateReceiptItems(items);
+  
+  expect(actualResult).toEqual(expectedResult);
+  });
+
 });
